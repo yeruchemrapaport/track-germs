6bb7920d1b964cadced325013b1c9a35636d1fbe
vulvy91703 <vulvy91703@gmail.com>
2026-01-12 15:01:11 +0200

    added check constraint to germ host

--- 1 tables.sql
+++ 1 tables.sql
@@ -6,8 +6,8 @@ create database TrackGermDB
 go
 use TrackGermDB
 go
-create table dbo.germ(
+create table dbo.Germ(
 	GermId int not null identity primary key,
-	GermName varchar(200) not null unique,
-	Host varchar(20) not null
+	GermName varchar(200) not null constraint u_germ_name_must_be_unique unique,
+	Host varchar(20) not null check (host in ('human','plant','animal')) 
 )
